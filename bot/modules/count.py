77732c62d14108244a31e3eacd0b0049f8ca8d7f
AJ786-GITHUB <91322672+AJ786-GITHUB@users.noreply.github.com>
2022-02-10 20:33:51 +0530

    Update count.py

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -1,5 +1,5 @@
-# Implement By - @anasty17 (https://github.com/SlamDevs/slam-mirrorbot/pull/111)
-# (c) https://github.com/SlamDevs/slam-mirrorbot
+# Implement By - @anasty17 (https://github.com/AJ786-GITHUB/slam-mirrorbot/pull/111)
+# (c) https://github.com/AJ786-GITHUB/slam-mirrorbot
 # All rights reserved
 
 from telegram.ext import CommandHandler
